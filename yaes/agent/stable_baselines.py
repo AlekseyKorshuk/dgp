63ab37c87100d3a0d03efabf34916b90ec69e7ec
Aleksey Korshuk <ale-kor02@mail.ru>
2022-12-03 16:27:59 +0300

    updates

--- yaes/agent/stable_baselines.py
+++ yaes/agent/stable_baselines.py
@@ -1,7 +1,20 @@
+from pathlib import Path
+
+import numpy as np
 from stable_baselines3.common.evaluation import evaluate_policy
 
 from yaes.environment import Environment
-from stable_baselines3 import DQN
+from stable_baselines3.common.callbacks import BaseCallback
+
+
+class RewardLogger(BaseCallback):
+    """Logs the reward accumulated in each episode"""
+
+    def _on_step(self) -> bool:
+        '''
+        Log my_custom_reward every _log_freq(th) to tensorboard for each environment
+        '''
+        print(self.locals)
 
 
 class RLAgent:
@@ -11,7 +24,8 @@ class RLAgent:
         self.train_args = train_args
 
     def train(self):
-        self.agent.learn(**self.train_args)
+        data = self.agent.learn(**self.train_args, callback=RewardLogger())
+        print(data)
         return self.agent
 
     def predict(self, state):
